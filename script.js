c93b5b783d9547b384f3fe32b0949ac540f6cc4f
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2024-11-13 11:40:42 +0530

    added nav animation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-gsap.to
+var tl = gsap.timeline()
+tl.from("h2", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+    delay: 0.3
+
+})
+
+tl.from("h4", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+    // stagger: 
+    // stagger: 0.3  
+    stagger: {
+        amount: 0.3,       // Total duration for staggering all elements
+        from: "center",  // Start staggering from the center of the group
+        ease: "power1.inOut", // Apply easing to the stagger
+      }
+
+})
