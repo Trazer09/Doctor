6ee91f779d9363d64d0ca06168f2064ad8f3a5de
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2025-04-12 16:59:47 +0530

    add public and protected route components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-// import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoutes";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
   return (
-
     <BrowserRouter>
-<Routes>                             
-<Route path = '/'  element ={<HomePage />} />
-<Route path = '/login'  element ={<Login />} />
-<Route path = '/register'  element ={<Register/>} />
-
-</Routes>
-</BrowserRouter>
-
-   
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
