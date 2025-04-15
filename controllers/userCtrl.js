706f1554a8634aaf3b4a05b9efc268b14620a1b2
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2025-04-15 16:35:47 +0530

    add applyDoctor.js

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -66,7 +66,8 @@ const loginController = async(req, res)=>{
 
 const authController = async (req, res) => {
   try {
-    const user = await userModel.findOne({ _id: req.body.userId });
+    const user = await userModel.findById({ _id: req.body.userId });
+    user.password = undefined;
     if (!user) {
       return res.status(200).send({
         message: "user not found",
@@ -75,9 +76,7 @@ const authController = async (req, res) => {
     } else {
       res.status(200).send({
         success: true,
-        data: {
-          name: user.name,
-          email: user.email,
+        data: { user,
         },
       });
     }
--- models/userModels.js
+++ models/userModels.js
@@ -21,7 +21,24 @@ password: {
 type: String,
 required: [true, 'password is required']
 
-}
+},
+
+isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isDoctor: {
+    type: Boolean,
+    default: false,
+  },
+  notifcation: {
+    type: Array,
+    default: [],
+  },
+  seennotification: {
+    type: Array,
+    default: [],
+  },
 
 
 
